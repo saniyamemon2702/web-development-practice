d61830548126ec0fbb86596f622d255bd5709530
Saniya Memon <saniyamemon7862@gmail.com>
2021-04-25 18:39:22 +0530

    Add files via upload

---
+++ js_pracitce_program/operators.js
@@ -0,0 +1,26 @@
+var a=20;
+var b=30;
+
+console.log('Arithmatic Operators');
+console.log(a,'+',b,'=',a+b);
+console.log(a,'-',b,'=',a-b);
+console.log(a,'*',b,'=',a*b);
+console.log(a,'/',b,'=',a/b);
+
+console.log('Comparision Operators');
+console.log(a,'<',b,'=',a<b);
+console.log(a,'>',b,'=',a>b);
+console.log(a,'<=',b,'=',a<=b);
+console.log(a,'>=',b,'=',a>=b);
+console.log(a,'!=',b,'=',a!=b);
+console.log(a,'==',b,'=',a==b);
+
+console.log('Identify Operators');
+console.log(a,'===',b,'--',a===b);
+console.log(a,'!==',b,'--',a!==b);
+
+
+
+
+
+
