b0e45ecf53b653b8c87e5d058190761bf60f2c23
Saniya Memon <saniyamemon7862@gmail.com>
2021-05-09 20:29:08 +0530

    checking whether number is ODD or EVEN

---
+++ js_pracitce_program/isEven.js
@@ -0,0 +1,11 @@
+function isEven(num){
+   
+   if(num%2===0){
+       console.log("EVEN");
+   }else if(num%2!==0){
+    console.log("ODD");
+   }else{
+       console.log("Unknown Number");
+   }
+
+}
