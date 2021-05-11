837c6b3c24372f79f53517a81ef9aede71077d24
Saniya Memon <saniyamemon7862@gmail.com>
2021-05-11 13:00:44 +0530

    array elements accecss and updation

---
+++ js_pracitce_program/array_operation.js
@@ -0,0 +1,5 @@
+const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix'];
+console.log("Before updating, array is :",leaderboard);
+leaderboard[1]='Luna';
+leaderboard[3]='Draco';
+console.log("After updating, array is :",leaderboard);
