c7bdf4fa5d88ac39f75574d2b18249ab1ee5efbb
Saniya Memon <saniyamemon7862@gmail.com>
2021-05-12 20:31:46 +0530

    Funtions ex-1

---
+++ js_pracitce_program/function_ex.js
@@ -0,0 +1,28 @@
+// Excrecise-1: Define printHeart function to print <3
+console.clear();
+function printHeart() {
+  console.log("<3");
+}
+
+printHeart();
+
+// Excrecise-2
+// console.clear();
+
+// // Excrecise-3
+// console.clear();
+
+// // Excrecise-4
+// console.clear();
+
+// // Excrecise-5
+// console.clear();
+
+// // Excrecise-6
+// console.clear();
+
+// // Excrecise-7
+// console.clear();
+
+// // Excrecise-8
+// console.clear();
