318433053792258f52b6bd866386c1b59ad25a27
Saniya Memon <saniyamemon7862@gmail.com>
2021-05-12 20:10:26 +0530

    JS part partially completed

--- smallProjects/todo/todo.js
+++ smallProjects/todo/todo.js
@@ -1,1 +1,13 @@
 console.log("successfully connected...");
+let tasks = ["Make cookies"];
+
+function addNewTask() {
+  let newTask = document.getElementById("newTask").value;
+  tasks.push(newTask);
+  console.log(tasks);
+  if (!newTask) {
+    alert("EMPTY TASK CAN NOT BE ADDED!!!");
+  } else {
+    alert("TASK SUCCESSFULLY ADDED");
+  }
+}
