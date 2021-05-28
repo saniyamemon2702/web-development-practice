ad01689483f05149357f354689abf062ec961b4a
Saniya Memon <saniyamemon7862@gmail.com>
2021-05-28 17:08:24 +0530

    HTML part completed...

---
+++ smallProjects/pokemon/index.js
@@ -0,0 +1,6 @@
+const conatiner = document.querySelector("#container");
+const baseURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+for (let i = 1; i <= 151; i++) {
+    const 
+}
