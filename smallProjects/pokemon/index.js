0c562a611b265352d930efb356d1b5e03b9a3ba9
Saniya Memon <saniyamemon7862@gmail.com>
2021-05-29 11:12:17 +0530

    Few changes in pokemon heading

--- smallProjects/pokemon/index.js
+++ smallProjects/pokemon/index.js
@@ -1,6 +1,17 @@
-const conatiner = document.querySelector("#container");
+const container = document.querySelector("#container");
 const baseURL =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-for (let i = 1; i <= 151; i++) {
-    const 
+
+const h1 = document.querySelector("h1");
+h1.style.textAlign = "center";
+
+for (let i = 1; i <= 800; i++) {
+  let newImg = document.createElement("img");
+
+  newImg.src = `${baseURL}${i}.png`;
+  container.appendChild(newImg);
+  const label = document.createElement("span");
+  label.textContent = i;
+  container.appendChild(newImg);
+  container.appendChild(label);
 }
