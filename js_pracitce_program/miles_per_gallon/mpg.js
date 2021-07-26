2be58d2ce7bd54abde971b1a53c5385180d040a6
Saniya Memon <saniyamemon7862@gmail.com>
2021-07-26 12:49:39 +0530

    Logic ready

---
+++ js_pracitce_program/miles_per_gallon/mpg.js
@@ -0,0 +1,5 @@
+var miles = parseFloat(prompt("Enter Miles Driven:"));
+var gallons = parseFloat(prompt("Enter gallons of Gas used:"));
+var mpg = miles / gallons;
+mpg = parseInt(mpg);
+alert("Miles per gallon = " + mpg);
