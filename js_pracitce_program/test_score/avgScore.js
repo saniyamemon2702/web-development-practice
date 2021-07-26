c833314d0172640fa2b8341d2013abf0b3724b37
Saniya Memon <saniyamemon7862@gmail.com>
2021-07-26 13:55:38 +0530

    Application completed!

--- js_pracitce_program/test_score/avgScore.js
+++ js_pracitce_program/test_score/avgScore.js
@@ -4,28 +4,29 @@ window.addEventListener("load", (event) => {
   var total = 0;
 
   //get 4 scores from user and add them together
-  entry = prompt("Enter test score");
-  var score1 = entry;
+  var score1 = parseFloat(prompt("Enter test score"));
   total = total + score1;
+  console.log("Score1:" + score1);
 
-  entry = prompt("Enter test score");
-  var score2 = entry;
+  var score2 = parseFloat(prompt("Enter test score"));
   total = total + score2;
+  console.log("Score2:" + score2);
 
-  entry = prompt("Enter test score");
-  var score3 = entry;
+  var score3 = parseFloat(prompt("Enter test score"));
   total = total + score3;
+  console.log("Score3:" + score3);
 
-  entry = prompt("Enter test score");
-  var score4 = entry;
+  var score4 = parseFloat(prompt("Enter test score"));
   total = total + score4;
+  console.log("Score4:" + score4);
 
+  console.log("Total:" + total);
   //calculate the average
-  average = total / 3;
+  average = total / 4;
 
   document.getElementById("score1").innerText = score1;
   document.getElementById("score2").innerText = score2;
   document.getElementById("score3").innerText = score3;
   document.getElementById("score4").innerText = score4;
-  document.getElementById("avg").innerText = average;
+  document.getElementById("avg").innerText = average.toFixed(2);
 });
