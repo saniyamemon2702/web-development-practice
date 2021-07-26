1733725b0f00d54c06f1ce4399e1e5bee6599c47
Saniya Memon <saniyamemon7862@gmail.com>
2021-07-26 13:10:27 +0530

    Changes in HTML file

--- js_pracitce_program/miles_per_gallon/mpg.js
+++ js_pracitce_program/miles_per_gallon/mpg.js
@@ -1,5 +1,11 @@
-var miles = parseFloat(prompt("Enter Miles Driven:"));
-var gallons = parseFloat(prompt("Enter gallons of Gas used:"));
-var mpg = miles / gallons;
-mpg = parseInt(mpg);
-alert("Miles per gallon = " + mpg);
+document.onload(() => {
+  var miles = parseFloat(prompt("Enter Miles Driven:"));
+  var gallons = parseFloat(prompt("Enter gallons of Gas used:"));
+  var mpg = miles / gallons;
+  mpg = parseInt(mpg);
+
+  document.getElementById("mile").nodeValue = miles;
+  document.getElementById("gallon").nodeValue = gallons;
+  document.getElementById("mpg").nodeValue = mpg;
+  //   alert("Miles per gallon = " + mpg);
+});
