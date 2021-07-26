3d4caf100a06c1cc9de245b3ab6ccd7679dc513e
Saniya Memon <saniyamemon7862@gmail.com>
2021-07-26 15:08:40 +0530

    Half of th applicatin completed!

--- js_pracitce_program/fututre_value/fv.js
+++ js_pracitce_program/fututre_value/fv.js
@@ -1,13 +1,25 @@
 window.addEventListener("load", (event) => {
+  var futureValue, interest;
+
+  //Amount
   var amount = parseFloat(prompt("Enter Investment Amount:"));
   document.getElementById("amt").innerText = amount;
   console.log("Amount:" + amount);
 
-  var interestRate = 7.5;
-  document.getElementById("ir").innerText = "7.5";
+  // Interest Rate
+  var interestRate = parseFloat(prompt("Enter Interest Rate:"));
+  document.getElementById("ir").innerText = interestRate;
   console.log("Rate:" + interestRate);
 
+  //Years
   var year = parseInt(prompt("Enter Years:"));
   document.getElementById("year").innerText = amount;
   console.log("Years:" + year);
+
+  for (i = 1; i <= year; i++) {
+    var html = `<p> Year ${i}=${interest} Value=${futureValue}`;
+    futureValue = amount;
+    futureValue = futureValue + (futureValue * interestRate) / 100;
+    document.querySelector(".container").appendChild(html);
+  }
 });
