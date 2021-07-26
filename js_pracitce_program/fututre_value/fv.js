87ffdf14ed14b66139c51f54a20dc2d96ac78fe4
Saniya Memon <saniyamemon7862@gmail.com>
2021-07-26 17:12:18 +0530

    Data Validation Added!

--- js_pracitce_program/fututre_value/fv.js
+++ js_pracitce_program/fututre_value/fv.js
@@ -1,29 +1,52 @@
 window.addEventListener("load", (event) => {
   var futureValue, interest;
+  while (1) {
+    //Amount
+    var amount = parseFloat(prompt("Enter Investment Amount:"));
+    document.getElementById("amt").innerText = amount;
+    console.log("Amount:" + amount);
+    if (amount <= 0) {
+      alert("Values must be positive");
+      continue;
+    } else if (isNaN(amount)) {
+      alert("Value must be integer!");
+      continue;
+    }
 
-  //Amount
-  var amount = parseFloat(prompt("Enter Investment Amount:"));
-  document.getElementById("amt").innerText = amount;
-  console.log("Amount:" + amount);
+    // Interest Rate
+    var interestRate = parseFloat(prompt("Enter Interest Rate:"));
+    document.getElementById("ir").innerText = interestRate;
+    console.log("Rate:" + interestRate);
+    if (interestRate <= 0) {
+      alert("Values must be positive");
+      continue;
+    } else if (isNaN(interestRate)) {
+      alert("Value must be integer!");
+      continue;
+    }
+    //Years
+    var year = parseInt(prompt("Enter Years:"));
+    document.getElementById("year").innerText = amount;
+    console.log("Years:" + year);
+    if (year <= 0) {
+      alert("Values must be positive");
+      continue;
+    } else if (isNaN(year)) {
+      alert("Value must be integer!");
+      continue;
+    }
 
-  // Interest Rate
-  var interestRate = parseFloat(prompt("Enter Interest Rate:"));
-  document.getElementById("ir").innerText = interestRate;
-  console.log("Rate:" + interestRate);
+    futureValue = amount;
+    for (i = 1; i <= year; i++) {
+      interest = (futureValue * interestRate) / 100;
+      futureValue += interest;
+      var html = `Year ${i}=${interest.toFixed(
+        2
+      )} Value=  ${futureValue.toFixed(2)}`;
 
-  //Years
-  var year = parseInt(prompt("Enter Years:"));
-  document.getElementById("year").innerText = amount;
-  console.log("Years:" + year);
-
-  futureValue = amount;
-  for (i = 1; i <= year; i++) {
-    interest = (futureValue * interestRate) / 100;
-    futureValue += interest;
-    var html = `Year ${i}=${interest} Value=${futureValue}`;
-
-    var p = document.createElement("p");
-    p.innerText = html;
-    document.querySelector(".container").append(p);
+      var p = document.createElement("p");
+      p.innerText = html;
+      document.querySelector(".container").append(p);
+    }
   }
 });
