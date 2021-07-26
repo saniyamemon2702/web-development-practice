8f378d72c03e65305ebb8aec371866f44f034d01
Saniya Memon <saniyamemon7862@gmail.com>
2021-07-26 17:31:06 +0530

    Enhanced Test Score Application Complited

--- js_pracitce_program/test_score - Enhanced/avgScore.js
+++ js_pracitce_program/test_score - Enhanced/avgScore.js
@@ -7,16 +7,11 @@ window.addEventListener("load", (event) => {
   for (i = 0; i < 4; i++) {
     entry[i] = parseFloat(prompt("Enter test score"));
     total = total + entry[i];
+    document.getElementById("score" + (i + 1)).innerText = entry[i];
     console.log("Score" + i + ":" + entry[i]);
-    document.getElementById(`score${i}`).innerText = entry[i];
   }
   //calculate the average
   average = total / 4;
-
-  // document.getElementById("score1").innerText = score1;
-  // document.getElementById("score2").innerText = score2;
-  // document.getElementById("score3").innerText = score3;
-  // document.getElementById("score4").innerText = score4;
   document.getElementById("avg").innerText = average.toFixed(2);
   document.getElementById("maxScore").innerText = Math.max(...entry);
 });
